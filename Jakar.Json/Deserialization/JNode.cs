2cbf398b23add72dff3da7eeedc683381b9106eb
Tyler Stegmaier <jakar510@gmail.com>
2022-04-26 22:21:30 -0500

    trying to figure out json deserialization

--- Jakar.Json/Deserialization/JNode.cs
+++ Jakar.Json/Deserialization/JNode.cs
@@ -1,4 +1,9 @@
-﻿namespace Jakar.Json.Deserialization;
+﻿using Jakar.Extensions.SpanAndMemory;
+using Newtonsoft.Json.Linq;
+
+
+
+namespace Jakar.Json.Deserialization;
 
 
 [SuppressMessage("ReSharper", "InconsistentNaming")]
@@ -11,7 +16,42 @@ public readonly ref struct JNode
     public NodeEnumerator GetChildren() => new(_span);
 
 
+    // public static JToken Parse( string json, JsonLoadSettings? settings )
+    // {
+    //     JsonReader reader = new JsonTextReader(new StringReader(json));
+    //     reader = new JsonTextReader(new StreamReader(json));
+    //
+    //     using ( reader )
+    //     {
+    //         JToken t = JToken.Load(reader, settings);
+    //
+    //         while ( reader.Read() )
+    //         {
+    //             // Any content encountered here other than a comment will throw in the reader.
+    //         }
+    //
+    //         return t;
+    //     }
+    // }
 
+
+
+    /*
+{
+    "test": {
+        "one": 1,
+        "two": 2
+    }
+}
+
+    
+[
+    "test": {
+        "one": 1,
+        "two": 2
+    }
+]
+     */
     public ref struct NodeEnumerator
     {
         private readonly ReadOnlySpan<char> _json;
@@ -24,7 +64,25 @@ public readonly ref struct JNode
         public void Reset() => _span = _json;
 
 
-        public bool MoveNext() { return false; }
+        public bool MoveNext()
+        {
+            _span = _span.Trim();
+
+            if ( Spans.IsNullOrWhiteSpace(_span) )
+            {
+                Current = default;
+                return false;
+            }
+
+            if ( _span.StartsWith('{') && _span.EndsWith('}') )
+            {
+                var start = _span.IndexOf('{');
+            }
+            else if ( _span.StartsWith('[') && _span.EndsWith(']') ) { }
+
+
+            return false;
+        }
         public void Dispose() { }
     }
 }
