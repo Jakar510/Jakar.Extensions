feb7909ae1b7fb97049e07e171f59a3af7b82ab0
Tyler Stegmaier <jakar510@gmail.com>
2022-05-10 16:38:31 -0500

    Jakar.Extensions.Xamarin.Forms 6.0.0.10-beta

--- Jakar.Extensions/Models/AppSettings.cs
+++ Jakar.Extensions/Models/AppSettings.cs
@@ -22,16 +22,15 @@ public interface IAppSettings<TViewPage> : IAppSettings
 
 
 [Serializable]
-public class AppSettings<TViewPage> : ObservableClass, IAppSettings<TViewPage>
+public class AppSettings : ObservableClass, IAppSettings
 {
     private string     _appName = string.Empty;
     private AppVersion _appVersion;
     private bool       _crashDataPending;
-    private TViewPage? _currentViewPage;
     private Guid       _deviceID;
     private string     _deviceVersion = string.Empty;
     private string?    _screenShotAddress;
-    private bool       _sendCrashes;
+    private bool       _sendCrashes = Preferences.Get(nameof(SendCrashes), true) || Debugger.IsAttached;
 
 
     public Guid DeviceID
@@ -43,19 +42,17 @@ public class AppSettings<TViewPage> : ObservableClass, IAppSettings<TViewPage>
     public bool SendCrashes
     {
         get => _sendCrashes;
-        set => SetProperty(ref _sendCrashes, value);
+        set
+        {
+            SetProperty(ref _sendCrashes, value);
+            Preferences.Set(nameof(SendCrashes), value);
+        }
     }
 
     public string? ScreenShotAddress
     {
         get => _screenShotAddress;
         set => SetProperty(ref _screenShotAddress, value);
-    }
-
-    public virtual TViewPage? CurrentViewPage
-    {
-        get => _currentViewPage;
-        set => SetProperty(ref _currentViewPage, value);
     }
 
     public string AppName
@@ -81,4 +78,32 @@ public class AppSettings<TViewPage> : ObservableClass, IAppSettings<TViewPage>
         get => _appVersion;
         set => SetProperty(ref _appVersion, value);
     }
+
+
+    public AppSettings() { }
+    public AppSettings( string appName, AppVersion version, string deviceVersion, Guid deviceID )
+    {
+        AppName       = appName;
+        AppVersion    = version;
+        DeviceVersion = deviceVersion;
+        DeviceID      = deviceID;
+    }
+}
+
+
+
+[Serializable]
+public class AppSettings<TViewPage> : AppSettings, IAppSettings<TViewPage>
+{
+    private TViewPage? _currentViewPage;
+
+    public virtual TViewPage? CurrentViewPage
+    {
+        get => _currentViewPage;
+        set => SetProperty(ref _currentViewPage, value);
+    }
+
+
+    public AppSettings() { }
+    public AppSettings( string appName, AppVersion version, string deviceVersion, Guid deviceID ) : base(appName, version, deviceVersion, deviceID) { }
 }
