aeed76e5423f115f5c83efc25b55b33df32f4c47
Tyler Stegmaier <jakar510@gmail.com>
2022-09-06 16:55:32 -0500

    added table linq

---
+++ Jakar.Database/TableLinq.cs
@@ -0,0 +1,18 @@
+﻿// Jakar.Extensions :: Jakar.Database
+// 09/06/2022  4:54 PM
+
+namespace Jakar.Database;
+
+
+public static class TableLinq
+{
+    public static async IAsyncEnumerable<TResult> Select<TRecord, TResult>( this IAsyncEnumerable<TRecord>                                                source,
+                                                                            Func<DbConnection, DbTransaction?, TRecord, CancellationToken, Task<TResult>> func,
+                                                                            DbConnection                                                                  connection,
+                                                                            DbTransaction?                                                                transaction,
+                                                                            [EnumeratorCancellation] CancellationToken                                    token = default
+    )
+    {
+        await foreach ( TRecord record in source.WithCancellation(token) ) { yield return await func(connection, transaction, record, token); }
+    }
+}
