5541cd6d8dd5ef48218bcf9c7723504fcd5f6430
Tyler Stegmaier <jakar510@gmail.com>
2023-02-25 22:43:12 -0600

    bug fixes

--- Jakar.Database/Tables/UserRights.cs
+++ Jakar.Database/Tables/UserRights.cs
@@ -6,41 +6,36 @@ namespace Jakar.Database;
 
 public abstract class UserRights : IEnumerator<(int Index, bool Value)>
 {
-    protected readonly char _valid;
-    protected readonly char _invalid;
-
-
-    protected          int          _index = 0;
-    protected readonly Memory<char> _rights;
+    protected int          _index = 0;
+    private   Memory<char> _rights;
 
 
     public (int Index, bool Value) Current => (_index, Has( _index ));
     public int                     Length  => _rights.Length;
     object IEnumerator.            Current => Current;
+    public Span<char>              Span    => _rights.Span;
+    public char                    Valid   { get; init; } = '+';
+    public char                    Invalid { get; init; } = '-';
 
 
-    private UserRights( char valid, char invalid )
-    {
-        _invalid = invalid;
-        _valid   = valid;
-    }
-    protected UserRights( string rights, char valid = '+', char invalid = '-' ) : this( valid, invalid )
+    public void Init( string rights )
     {
         Span<char> buffer = stackalloc char[rights.Length];
         rights.CopyTo( buffer );
         _rights = buffer.AsMemory();
     }
-    protected UserRights( int length, char valid = '+', char invalid = '-' ) : this( valid, invalid )
+    public void Init( int length )
     {
         Span<char> buffer = stackalloc char[length];
         _rights = buffer.AsMemory();
-        Span<char> span = _rights.Span;
 
-        for ( int i = 0; i < Length; i++ ) { span[i] = _invalid; }
+        for ( int i = 0; i < Length; i++ ) { Span[i] = Invalid; }
     }
+
+
     public virtual void Dispose()
     {
-        _rights.Span.Clear();
+        Span.Clear();
         GC.SuppressFinalize( this );
     }
 
@@ -49,23 +44,23 @@ public abstract class UserRights : IEnumerator<(int Index, bool Value)>
     public void Reset() => _index = -1;
 
 
-    public bool Has( int  index ) => _rights.Span[index] == _valid;
+    public bool Has( int  index ) => Span[index] == Valid;
     public bool Has<T>( T index ) where T : struct, Enum => Has( index.AsInt() );
 
 
-    public void Remove( int  index ) => Set( index, _invalid );
+    public void Remove( int  index ) => Set( index, Invalid );
     public void Remove<T>( T index ) where T : struct, Enum => Remove( index.AsInt() );
 
 
-    public void Add( int  index ) => Set( index, _valid );
+    public void Add( int  index ) => Set( index, Valid );
     public void Add<T>( T index ) where T : struct, Enum => Add( index.AsInt() );
 
 
     protected void Set( int index, char right )
     {
-        if ( right != _valid && right != _invalid ) { throw new ArgumentException( $"{nameof(right)} must be one of [ {_valid}, {_invalid} ]" ); }
+        if ( right != Valid && right != Invalid ) { throw new ArgumentException( $"{nameof(right)} must be one of [ {Valid}, {Invalid} ]" ); }
 
-        _rights.Span[index] = right;
+        Span[index] = right;
     }
 
 
