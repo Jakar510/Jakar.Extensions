073a21268a888e24fb836cf0239dd30e69826e50
Tyler Stegmaier <jakar510@gmail.com>
2023-03-03 22:49:04 -0600

    bug fixes

--- Jakar.Database/Models/LoginResult.cs
+++ Jakar.Database/Models/LoginResult.cs
@@ -26,63 +26,109 @@ public readonly struct LoginResult
     public static implicit operator LoginResult( UserRecord           result ) => new(result);
 
 
-    public bool GetResult( [NotNullWhen( true )] out ActionResult? actionResult, [NotNullWhen( false )] out UserRecord? caller )
+    public ActionResult? GetResult()
+    {
+        return Result switch
+               {
+                   State.Success => default,
+                   State.UnknownError when Model is not null => new UnprocessableEntityObjectResult( Model )
+                                                                {
+                                                                    StatusCode = (int)Status.InternalServerError
+                                                                },
+                   State.UnknownError when Exception is not null => new UnprocessableEntityObjectResult( new ProblemDetails
+                                                                                                         {
+                                                                                                             Detail   = Exception.Message,
+                                                                                                             Instance = Exception.Source,
+                                                                                                             Status   = (int)Status.InternalServerError,
+                                                                                                             Title    = Exception.MethodName(),
+                                                                                                             Type = Exception.GetType()
+                                                                                                                             .Name
+                                                                                                         } )
+                                                                    {
+                                                                        StatusCode = (int)Status.InternalServerError
+                                                                    },
+                   State.UnknownError => new UnprocessableEntityObjectResult( default(object) )
+                                         {
+                                             StatusCode = (int)Status.InternalServerError
+                                         },
+                   State.BadCredentials => new UnauthorizedObjectResult( Result )
+                                           {
+                                               StatusCode = (int)Status.Unauthorized
+                                           },
+                   State.Inactive => new UnauthorizedObjectResult( Result )
+                                     {
+                                         StatusCode = (int)Status.Forbidden
+                                     },
+                   State.Locked => new UnauthorizedObjectResult( Result )
+                                   {
+                                       StatusCode = (int)Status.Locked
+                                   },
+                   State.Disabled => new UnauthorizedObjectResult( Result )
+                                     {
+                                         StatusCode = (int)Status.Disabled
+                                     },
+                   State.ExpiredSubscription => new BadRequestObjectResult( Result )
+                                                {
+                                                    StatusCode = (int)Status.PaymentRequired
+                                                },
+                   State.NoSubscription => new BadRequestObjectResult( Result )
+                                           {
+                                               StatusCode = (int)Status.PaymentRequired
+                                           },
+                   _ => throw new OutOfRangeException( nameof(Result), Result ),
+               };
+    }
+    public bool GetResult( [NotNullWhen( false )] out UserRecord? caller )
     {
         caller = User;
-
-        actionResult = Result switch
-                       {
-                           State.Success => default,
-                           State.UnknownError when Model is not null => new UnprocessableEntityObjectResult( Model )
-                                                                        {
-                                                                            StatusCode = (int)Status.InternalServerError
-                                                                        },
-                           State.UnknownError when Exception is not null => new UnprocessableEntityObjectResult( new ProblemDetails
-                                                                                                                 {
-                                                                                                                     Detail   = Exception.Message,
-                                                                                                                     Instance = Exception.Source,
-                                                                                                                     Status   = (int)Status.InternalServerError,
-                                                                                                                     Title    = Exception.MethodName(),
-                                                                                                                     Type = Exception.GetType()
-                                                                                                                                     .Name
-                                                                                                                 } )
-                                                                            {
-                                                                                StatusCode = (int)Status.InternalServerError
-                                                                            },
-                           State.UnknownError => new UnprocessableEntityObjectResult( default(object) )
-                                                 {
-                                                     StatusCode = (int)Status.InternalServerError
-                                                 },
-                           State.BadCredentials => new UnauthorizedObjectResult( Result )
-                                                   {
-                                                       StatusCode = (int)Status.Unauthorized
-                                                   },
-                           State.Inactive => new UnauthorizedObjectResult( Result )
-                                             {
-                                                 StatusCode = (int)Status.Forbidden
-                                             },
-                           State.Locked => new UnauthorizedObjectResult( Result )
-                                           {
-                                               StatusCode = (int)Status.Locked
-                                           },
-                           State.Disabled => new UnauthorizedObjectResult( Result )
-                                             {
-                                                 StatusCode = (int)Status.Disabled
-                                             },
-                           State.ExpiredSubscription => new BadRequestObjectResult( Result )
-                                                        {
-                                                            StatusCode = (int)Status.PaymentRequired
-                                                        },
-                           State.NoSubscription => new BadRequestObjectResult( Result )
-                                                   {
-                                                       StatusCode = (int)Status.PaymentRequired
-                                                   },
-                           _ => throw new OutOfRangeException( nameof(Result), Result ),
-                       };
-
-
+        return caller is null;
+    }
+    public bool GetResult( [NotNullWhen( true )] out ActionResult? actionResult )
+    {
+        actionResult = GetResult();
+        return actionResult is not null;
+    }
+    public bool GetResult( [NotNullWhen( true )] out ActionResult? actionResult, [NotNullWhen( false )] out UserRecord? caller )
+    {
+        actionResult = GetResult();
+        caller       = User;
         return actionResult is not null && caller is null;
     }
+
+
+    public ModelStateDictionary GetModelStateDictionary()
+    {
+        var dictionary = Model ?? new ModelStateDictionary();
+        dictionary.AddModelError( nameof(State), Result.ToString() );
+
+        dictionary.AddModelError( nameof(Status),
+                                  GetStatus()
+                                     .ToStringFast() );
+
+        if ( Exception is null ) { return dictionary; }
+
+        dictionary.AddModelError( nameof(ProblemDetails.Detail),   Exception.Message );
+        dictionary.AddModelError( nameof(ProblemDetails.Instance), Exception.Source ?? string.Empty );
+        dictionary.AddModelError( nameof(ProblemDetails.Title),    Exception.MethodName() ?? string.Empty );
+
+        dictionary.AddModelError( nameof(ProblemDetails.Type),
+                                  Exception.GetType()
+                                           .Name );
+
+        return dictionary;
+    }
+    public Status GetStatus() => Result switch
+                                 {
+                                     State.Success             => Status.Ok,
+                                     State.UnknownError        => Status.InternalServerError,
+                                     State.BadCredentials      => Status.Unauthorized,
+                                     State.Inactive            => Status.Forbidden,
+                                     State.Locked              => Status.Locked,
+                                     State.Disabled            => Status.Disabled,
+                                     State.ExpiredSubscription => Status.PaymentRequired,
+                                     State.NoSubscription      => Status.PaymentRequired,
+                                     _                         => throw new OutOfRangeException( nameof(Result), Result ),
+                                 };
 
 
 
