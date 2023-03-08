11a0594febcf8038d99a936aa66d7aced1fb8301
Tyler Stegmaier <jakar510@gmail.com>
2023-03-08 17:46:01 -0600

    Updated AsyncLinq.Random and added AsyncLinq.ToArray

--- Jakar.Extensions/Linqs/AsyncLinq.Random.cs
+++ Jakar.Extensions/Linqs/AsyncLinq.Random.cs
@@ -5,23 +5,34 @@
 
 public static partial class AsyncLinq
 {
-    public static IEnumerable<TElement> Random<TElement>( this IReadOnlyList<TElement> items, Random rand, CancellationToken token = default )
+    public static TElement[] ToArray<TElement>( this IReadOnlyList<TElement> source )
     {
-        while ( token.ShouldContinue() ) { yield return items[rand.Next( items.Count )]; }
-    }
-    public static IEnumerable<TElement> RandomValues<TKey, TElement>( this IDictionary<TKey, TElement> dict, Random rand, CancellationToken token = default )
-    {
-        // ReSharper disable once LoopCanBeConvertedToQuery
-        foreach ( TKey key in dict.RandomKeys( rand, token ) ) { yield return dict[key]; }
+    #if NET6_0_OR_GREATER
+        TElement[] array = GC.AllocateUninitializedArray<TElement>( source.Count );
+    #else
+        TElement[] array = new TElement[source.Count];
+    #endif
+
+        for ( int i = 0; i < array.Length; i++ ) { array[i] = source[i]; }
+
+        return array;
     }
 
 
-    public static IEnumerable<TKey> RandomKeys<TKey, TElement>( this IDictionary<TKey, TElement> dict, Random rand, CancellationToken token = default )
+    public static IEnumerable<TElement> Random<TElement>( this IReadOnlyList<TElement> items, Random random, CancellationToken token = default )
     {
-        while ( token.ShouldContinue() )
-        {
-            yield return dict.ElementAt( rand.Next( dict.Count ) )
-                             .Key;
-        }
+        while ( token.ShouldContinue() ) { yield return items[random.Next( items.Count )]; }
     }
+    public static IEnumerable<TElement> Random<TElement>( this IReadOnlyList<TElement> items, Random random, int count, CancellationToken token = default ) => items.Random( random, token )
+                                                                                                                                                                    .Take( count );
+
+
+    public static IEnumerable<KeyValuePair<TKey, TElement>> Random<TKey, TElement>( this IReadOnlyDictionary<TKey, TElement> dict, Random random, CancellationToken token = default )
+    {
+        while ( token.ShouldContinue() ) { yield return dict.ElementAt( random.Next( dict.Count ) ); }
+    }
+    public static IEnumerable<TElement> RandomValues<TKey, TElement>( this IReadOnlyDictionary<TKey, TElement> dict, Random random, CancellationToken token = default ) => dict.Random( random, token )
+                                                                                                                                                                               .Select( pair => pair.Value );
+    public static IEnumerable<TKey> RandomKeys<TKey, TElement>( this IReadOnlyDictionary<TKey, TElement> dict, Random random, CancellationToken token = default ) => dict.Random( random, token )
+                                                                                                                                                                         .Select( pair => pair.Key );
 }
