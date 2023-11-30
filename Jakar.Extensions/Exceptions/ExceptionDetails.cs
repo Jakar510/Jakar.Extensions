6d1650165fb07d668f1a90c7b63b62865d1cf4f7
Tyler Stegmaier <jakar510@gmail.com>
2023-11-30 12:45:03 -0600

    ExceptionDetails updates

--- Jakar.Extensions/Exceptions/ExceptionDetails.cs
+++ Jakar.Extensions/Exceptions/ExceptionDetails.cs
@@ -1,7 +1,11 @@
-﻿namespace Jakar.Extensions;
+﻿using System.Text.Json.Serialization;
 
 
-public class ExceptionDetails
+
+namespace Jakar.Extensions;
+
+
+public sealed class ExceptionDetails
 {
     public                               Dictionary<string, JToken?>? Data            { get; init; }
     [ MaxLength( 4096 ) ] public         string?                      HelpLink        { get; init; }
@@ -10,7 +14,7 @@ public class ExceptionDetails
     [ MaxLength( int.MaxValue ) ] public string                       Message         { get; init; } = string.Empty;
     [ MaxLength( 10240 ) ]        public string?                      MethodSignature { get; init; }
     [ MaxLength( 4096 ) ]         public string?                      Source          { get; init; }
-    public                               List<string>?                StackTrace      { get; init; }
+    public                               string[]                     StackTrace      { get; init; } = Array.Empty<string>();
     [ MaxLength( 4096 ) ] public         string                       Str             { get; init; } = string.Empty;
     public                               MethodDetails?               TargetSite      { get; init; }
     [ MaxLength( 4096 ) ] public         string?                      Type            { get; init; }
@@ -27,7 +31,7 @@ public class ExceptionDetails
         HelpLink = e.HelpLink;
         Source   = e.Source;
 
-        StackTrace = e.StackTrace?.SplitAndTrimLines().ToList();
+        StackTrace = e.StackTrace?.SplitAndTrimLines().ToArray() ?? Array.Empty<string>();
 
         MethodSignature = $"{e.MethodClass()}::{e.MethodSignature()}";
         Data            = e.GetData();
@@ -45,30 +49,29 @@ public class ExceptionDetails
 
 
 
-public class MethodDetails
+public sealed class MethodDetails
 {
-    public                        MethodAttributes        Attributes          { get; init; }
-    [ MaxLength( 10240 ) ] public string?                 DeclaringType       { get; init; }
-    public                        bool                    IsAbstract          { get; init; }
-    public                        bool                    IsAssembly          { get; init; }
-    public                        bool                    IsConstructor       { get; init; }
-    public                        bool                    IsFamily            { get; init; }
-    public                        bool                    IsFamilyAndAssembly { get; init; }
-    public                        bool                    IsFamilyOrAssembly  { get; init; }
-    public                        bool                    IsFinal             { get; init; }
-    public                        bool                    IsPrivate           { get; init; }
-    public                        bool                    IsPublic            { get; init; }
-    public                        bool                    IsSpecialName       { get; init; }
-    public                        bool                    IsStatic            { get; init; }
-    public                        bool                    IsVirtual           { get; init; }
-    [ MaxLength( 4096 ) ] public  string                  Name                { get; init; } = string.Empty;
-    public                        List<ParameterDetails>? Parameters          { get; init; }
-    [ MaxLength( 10240 ) ] public string                  Signature           { get; init; } = string.Empty;
+    public                        MethodAttributes   Attributes          { get; init; }
+    [ MaxLength( 10240 ) ] public string?            DeclaringType       { get; init; }
+    public                        bool               IsAbstract          { get; init; }
+    public                        bool               IsAssembly          { get; init; }
+    public                        bool               IsConstructor       { get; init; }
+    public                        bool               IsFamily            { get; init; }
+    public                        bool               IsFamilyAndAssembly { get; init; }
+    public                        bool               IsFamilyOrAssembly  { get; init; }
+    public                        bool               IsFinal             { get; init; }
+    public                        bool               IsPrivate           { get; init; }
+    public                        bool               IsPublic            { get; init; }
+    public                        bool               IsSpecialName       { get; init; }
+    public                        bool               IsStatic            { get; init; }
+    public                        bool               IsVirtual           { get; init; }
+    [ MaxLength( 4096 ) ] public  string             Name                { get; init; } = string.Empty;
+    public                        ParameterDetails[] Parameters          { get; init; } = Array.Empty<ParameterDetails>();
+    [ MaxLength( 10240 ) ] public string             Signature           { get; init; } = string.Empty;
 
 
     public MethodDetails() { }
     public MethodDetails( Exception e ) : this( e.TargetSite ?? throw new NullReferenceException( nameof(e.TargetSite) ) ) { }
-
     public MethodDetails( MethodBase method )
     {
         DeclaringType       = method.MethodClass();
@@ -93,7 +96,7 @@ public class MethodDetails
 
 
 
-public class ParameterDetails
+public sealed class ParameterDetails
 {
     public                        bool    HasDefaultValue { get; init; }
     public                        bool    IsIn            { get; init; }
@@ -117,6 +120,10 @@ public class ParameterDetails
     }
 
 
-    public static List<ParameterDetails> Create( MethodBase                 method ) => Create( method.GetParameters() );
-    public static List<ParameterDetails> Create( IEnumerable<ParameterInfo> items )  => new(items.Select( x => new ParameterDetails( x ) ));
+    public static ParameterDetails[] Create( MethodBase                 method ) => Create( method.GetParameters() );
+    public static ParameterDetails[] Create( IEnumerable<ParameterInfo> items )  => items.Select( x => new ParameterDetails( x ) ).ToArray();
 }
+
+
+
+// [ JsonConverter( typeof(ParameterDetails) ) ] public partial class ParameterDetailsContext : JsonSerializerContext { }
