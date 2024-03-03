07be1230ec888c32623c1aee23e0ebc46022575f
Tyler Stegmaier <jakar510@gmail.com>
2024-03-03 11:31:27 -0600

    SQL builder fixes

--- Jakar.Database/DbMigrations/Api/SqlTableBuilder.cs
+++ Jakar.Database/DbMigrations/Api/SqlTableBuilder.cs
@@ -1,6 +1,10 @@
 ﻿// Jakar.Extensions :: Jakar.SqlBuilder
 // 3/1/2024  23:20
 
+using FluentMigrator.Model;
+
+
+
 namespace Jakar.Database.DbMigrations;
 
 
@@ -50,88 +54,197 @@ namespace Jakar.Database.DbMigrations;
        POINT, LINESTRING, POLYGON, and more: These types are used for storing and manipulating spatial data, such as coordinates and shapes, especially in databases that support GIS (Geographic Information Systems).
  */
 public ref struct SqlTableBuilder<TRecord>
-    where TRecord : TableRecord<TRecord>, IDbReaderMapping<TRecord>
+    where TRecord : ITableRecord<TRecord>, IDbReaderMapping<TRecord>
 {
-    private static readonly string             NULL       = "NULL";
-    private static readonly string             TABLE_NAME = TRecord.TableName;
-    private                 ValueStringBuilder _query     = new(10240);
+    public const     int                ANSI_STRING_CAPACITY    = 8000;
+    public const     int                ANSI_TEXT_CAPACITY      = 2147483647;
+    public const     int                UNICODE_STRING_CAPACITY = 4000;
+    public const     int                UNICODE_TEXT_CAPACITY   = 1073741823;
+    public const     int                IMAGE_CAPACITY          = 2147483647;
+    public const     int                DECIMAL_CAPACITY        = 38;
+    private readonly DbInstance         _instance;
+    private          ValueStringBuilder _query = new(10240);
 
 
-    public SqlTableBuilder() { }
-    public SqlTableBuilder<TRecord> Create( string tableName )
+    public SqlTableBuilder( DbInstance instance )
     {
-        _query.Append( $"CREATE TABLE {tableName} (" );
-        return this;
+        _instance = instance;
+        _query.Append( $"CREATE TABLE {TRecord.TableName} (" );
     }
+    public static SqlTableBuilder<TRecord> Create() => new();
+
 
     public SqlTableBuilder<TRecord> WithColumn<T>( string columnName, int? size = null, bool isPrimaryKey = false )
     {
-        string dataType = GetDataType<T>( out bool isNullable, size );
+        DbType dbType = GetDataType<T>( out bool isNullable, ref size );
+        return WithColumn( columnName, dbType, isNullable, size, isPrimaryKey );
+    }
+    public SqlTableBuilder<TRecord> WithColumn( string columnName, DbType dbType, bool isNullable, int? size = null, bool isPrimaryKey = false )
+    {
+        ColumnDefinition definition = new();
+        string           dataType   = GetDataType( _instance, dbType, size );
 
         _query.Append( _query.Span.EndsWith( "(" )
-                           ? $"{columnName} {dataType}"
+                           ? $" {columnName} {dataType}"
                            : $", {columnName} {dataType}" );
 
         if ( isPrimaryKey ) { _query.Append( " PRIMARY KEY" ); }
 
-        if ( isNullable ) { }
+        if ( isNullable ) { _query.Append( " NOT NULL" ); }
 
+        _query.Append( '\n' );
         return this;
     }
-    private static string GetDataType<T>( out bool isNullable, int? size = null )
+
+
+    private static DbType GetDataType<T>( out bool isNullable, ref int? size )
     {
         Type type = typeof(T);
         isNullable = type.IsNullableType() || type.IsBuiltInNullableType();
 
-        if ( type.IsEnum ) { return "CHAR(255)"; }
-
-        if ( type == typeof(string) )
+        if ( type.IsEnum )
         {
-            return size.HasValue
-                       ? $"VARCHAR({size})"
-                       : "CHAR(256)";
+            size = Spans.Max<string, int>( Enum.GetNames( typeof(T) ), static x => x.Length, 0 );
+            return DbType.StringFixedLength;
         }
 
-        if ( type == typeof(byte[]) )
-        {
-            return size.HasValue
-                       ? $"VARBINARY({size})"
-                       : "BLOB";
-        }
+        if ( type == typeof(string) ) { return DbType.String; }
 
-        if ( typeof(JToken).IsAssignableFrom( type ) || type == typeof(IDictionary<string, JToken?>) ) { return "JSON"; }
+        if ( type == typeof(byte[]) ) { return DbType.Binary; }
 
-        if ( type == typeof(Guid) || type == typeof(RecordID<TRecord>) ) { return "GUID"; }
+        if ( typeof(JToken).IsAssignableFrom( type ) || typeof(IDictionary<string, JToken?>).IsAssignableFrom( type ) ) { return DbType.Object; }
 
-        if ( type == typeof(byte) ) { return "TINYINT"; }
+        if ( type == typeof(Guid) || type == typeof(RecordID<TRecord>) ) { return DbType.Guid; }
 
-        if ( type == typeof(short) ) { return "SMALLINT"; }
+        if ( type == typeof(byte) ) { return DbType.Byte; }
 
-        if ( type == typeof(int) ) { return "INT"; }
+        if ( type == typeof(short) ) { return DbType.Int16; }
 
-        if ( type == typeof(long) ) { return "BIGINT"; }
+        if ( type == typeof(ushort) ) { return DbType.UInt16; }
 
-        if ( type == typeof(float) ) { return "FLOAT(9)"; }
+        if ( type == typeof(int) ) { return DbType.Int32; }
 
-        if ( type == typeof(double) ) { return "REAL"; }
+        if ( type == typeof(uint) ) { return DbType.UInt32; }
 
-        if ( type == typeof(decimal) ) { return "DECIMAL(29, 29)"; }
+        if ( type == typeof(long) ) { return DbType.Int64; }
 
-        if ( type == typeof(bool) ) { return "BOOLEAN"; }
+        if ( type == typeof(ulong) ) { return DbType.UInt64; }
 
-        if ( type == typeof(DateOnly) ) { return "DATE"; }
+        if ( type == typeof(float) ) { return DbType.Single; }
 
-        if ( type == typeof(TimeOnly) ) { return "TIME"; }
+        if ( type == typeof(double) ) { return DbType.Double; }
 
-        if ( type == typeof(TimeSpan) ) { return "DATETIME"; }
+        if ( type == typeof(decimal) ) { return DbType.Decimal; }
 
-        if ( type == typeof(DateTime) ) { return "DATETIME"; }
+        if ( type == typeof(bool) ) { return DbType.Boolean; }
 
-        if ( type == typeof(DateTimeOffset) ) { return "TIMESTAMP"; }
+        if ( type == typeof(DateOnly) ) { return DbType.Date; }
 
+        if ( type == typeof(TimeOnly) ) { return DbType.Time; }
+
+        if ( type == typeof(TimeSpan) ) { return DbType.Time; }
+
+        if ( type == typeof(DateTime) ) { return DbType.DateTime2; }
+
+        if ( type == typeof(DateTimeOffset) ) { return DbType.DateTimeOffset; }
 
         throw new ArgumentException( $"Unsupported type: {type.Name}" );
     }
+
+    private static string GetDataType( in DbInstance instance, in DbType dbType, in int? size = null )
+    {
+        return instance switch
+               {
+                   DbInstance.MsSql    => GetDataTypeSqlServer( dbType, size ),
+                   DbInstance.Postgres => GetDataTypePostgresSql( dbType, size ),
+                   _                   => throw new OutOfRangeException( nameof(instance), instance )
+               };
+    }
+
+    [SuppressMessage( "ReSharper", "StringLiteralTypo" )]
+    private static string GetDataTypePostgresSql( in DbType dbType, in int? size )
+    {
+        return dbType switch
+               {
+                   DbType.Binary => size.HasValue
+                                        ? $"VARBINARY({size})"
+                                        : "BLOB",
+                   DbType.SByte => "bytea",
+                   DbType.Byte  => "bytea",
+                   DbType.AnsiString => size.HasValue
+                                            ? $"varchar({size})"
+                                            : "varchar(MAX)",
+                   DbType.String => size.HasValue
+                                        ? $"text({size})"
+                                        : "text(MAX)",
+                   DbType.AnsiStringFixedLength => $"char({size})",
+                   DbType.StringFixedLength     => $"char({size})",
+                   DbType.Guid                  => "uuid",
+                   DbType.Int16                 => "smallint",
+                   DbType.Int32                 => "integer",
+                   DbType.Int64                 => "bigint",
+                   DbType.UInt16                => "smallint",
+                   DbType.UInt32                => "integer",
+                   DbType.UInt64                => "bigint",
+                   DbType.Single                => "float4",
+                   DbType.Double                => "float8",
+                   DbType.Decimal               => "decimal(19, 5)",
+                   DbType.VarNumeric            => "decimal(19, 5)",
+                   DbType.Boolean               => "bool",
+                   DbType.Date                  => "date",
+                   DbType.Time                  => "time",
+                   DbType.DateTime              => "timestamp",
+                   DbType.DateTime2             => "timestamp",
+                   DbType.DateTimeOffset        => "timestamptz",
+                   DbType.Currency              => "money",
+                   DbType.Object                => "json",
+                   DbType.Xml                   => "xml",
+                   _                            => throw new OutOfRangeException( nameof(dbType), dbType )
+               };
+    }
+
+    [SuppressMessage( "ReSharper", "StringLiteralTypo" )]
+    private static string GetDataTypeSqlServer( in DbType dbType, int? size )
+    {
+        return dbType switch
+               {
+                   DbType.Binary => size.HasValue
+                                        ? $"VARBINARY({size})"
+                                        : "BLOB",
+                   DbType.SByte => "TINYINT",
+                   DbType.Byte  => "TINYINT",
+                   DbType.AnsiString => size.HasValue
+                                            ? $"NVARCHAR({size})"
+                                            : "NVARCHAR(MAX)",
+                   DbType.String => size.HasValue
+                                        ? $"NTEXT({size})"
+                                        : "NTEXT(MAX)",
+                   DbType.AnsiStringFixedLength => $"CHAR({size})",
+                   DbType.StringFixedLength     => $"NCHAR({size})",
+                   DbType.Guid                  => "GUID",
+                   DbType.Int16                 => "SMALLINT",
+                   DbType.Int32                 => "INT",
+                   DbType.Int64                 => "BIGINT",
+                   DbType.UInt16                => "SMALLINT",
+                   DbType.UInt32                => "INT",
+                   DbType.UInt64                => "BIGINT",
+                   DbType.Single                => "FLOAT(9)",
+                   DbType.Double                => "DOUBLE PRECISION",
+                   DbType.Decimal               => "DECIMAL(19, 5)",
+                   DbType.VarNumeric            => "DECIMAL(19, 5)",
+                   DbType.Boolean               => "BOOLEAN",
+                   DbType.Date                  => "DATE",
+                   DbType.Time                  => "TIME",
+                   DbType.DateTime              => "DATETIME2",
+                   DbType.DateTime2             => "DATETIME2",
+                   DbType.DateTimeOffset        => "TIMESTAMP",
+                   DbType.Currency              => "MONEY",
+                   DbType.Object                => "JSON",
+                   DbType.Xml                   => "XML",
+                   _                            => throw new OutOfRangeException( nameof(dbType), dbType )
+               };
+    }
+
 
     public SqlTableBuilder<TRecord> Modify( string tableName )
     {
@@ -139,11 +252,13 @@ public ref struct SqlTableBuilder<TRecord>
         _query.Append( $"ALTER TABLE {tableName} " );
         return this;
     }
+
     public SqlTableBuilder<TRecord> AddColumn( string columnName, string dataType )
     {
         _query.Append( $"ADD COLUMN {columnName} {dataType}, " );
         return this;
     }
+
     public SqlTableBuilder<TRecord> DropColumn( string columnName )
     {
         _query.Append( $"DROP COLUMN {columnName}, " );
@@ -155,7 +270,7 @@ public ref struct SqlTableBuilder<TRecord>
         string query = _query.ToString().TrimEnd( ' ', ',' );
         if ( query.EndsWith( "(" ) ) { query = query.Remove( query.Length - 1 ); }
 
-        query += ");";
+        query += " );";
         return query;
     }
 }
