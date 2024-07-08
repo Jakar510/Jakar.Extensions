bb854e08c6d4107ed431c1851a08791fcc54c1e2
Tyler Stegmaier <jakar510@gmail.com>
2024-07-08 12:30:18 -0500

    remove Xamarin.Forms test project

--- TestAppIos/TestAppIos/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-using Xamarin.Forms.Xaml;
-
-
-[assembly: XamlCompilation( XamlCompilationOptions.Compile )]
--- TestAppIos/TestAppIos/Models/Item.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TestAppIos.Models
-{
-    public class Item
-    {
-        public string Description { get; set; }
-        public string Id          { get; set; }
-        public string Text        { get; set; }
-    }
-}
