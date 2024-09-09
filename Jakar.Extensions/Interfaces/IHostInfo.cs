0e54a11080ebb304976358f3db0ba8765cb3df60
Tyler Stegmaier <jakar510@gmail.com>
2024-09-09 09:10:03 -0500

    Jakar.Extensions v8.13.0

--- Jakar.Extensions/Interfaces/IHostInfo.cs
+++ Jakar.Extensions/Interfaces/IHostInfo.cs
@@ -1,10 +1,32 @@
 ﻿// Jakar.Extensions :: Jakar.Extensions
 // 08/03/2022  5:29 PM
 
+using Microsoft.Extensions.DependencyInjection;
+
+
+
 namespace Jakar.Extensions;
 
 
 public interface IHostInfo
 {
     public Uri HostInfo { get; }
+
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static IServiceCollection AddSingleton<TClass>( IServiceCollection collection, TClass value )
+        where TClass : class, IHostInfo => collection.AddSingleton( value ).AddTransient<IHostInfo, TClass>( static provider => provider.GetRequiredService<TClass>() );
+
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static IServiceCollection AddSingleton<TClass>( IServiceCollection collection, Func<IServiceProvider, TClass> value )
+        where TClass : class, IHostInfo => collection.AddSingleton( value ).AddTransient<IHostInfo, TClass>( static provider => provider.GetRequiredService<TClass>() );
+
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )]
+    public static IServiceCollection AddScoped<TClass>( IServiceCollection collection, Func<IServiceProvider, TClass> value )
+        where TClass : class, IHostInfo => collection.AddScoped( value ).AddTransient<IHostInfo, TClass>( static provider => provider.GetRequiredService<TClass>() );
+
+
+    [MethodImpl( MethodImplOptions.AggressiveInlining )] public static IHostInfo Get( IServiceProvider provider ) => provider.GetRequiredService<IHostInfo>();
 }
