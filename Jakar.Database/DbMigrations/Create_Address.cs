cb15a0ff1ed33901305f71fc2b8450a25e22e29d
Tyler Stegmaier <jakar510@gmail.com>
2024-09-17 23:01:22 -0500

    Jakar.Database v7.0.0.33-beta

---
+++ Jakar.Database/DbMigrations/Create_Address.cs
@@ -0,0 +1,15 @@
+﻿// Jakar.Extensions :: Jakar.Database
+// 09/17/2024  23:09
+
+namespace Jakar.Database.DbMigrations;
+
+
+[Migration( CREATE_ADDRESS )]
+
+// ReSharper disable once InconsistentNaming
+public sealed class Create_Address : Migrate_Address
+{
+    public Create_Address() : base() { }
+    public override void Up()   => CreateTable();
+    public override void Down() => DeleteTable();
+}
