708d856b72e683bc56931413c0971cfdcd4e52c5
Tyler Stegmaier <jakar510@gmail.com>
2025-01-21 09:38:35 -0600

    Added more DictionaryExtensions

--- Jakar.Extensions/Extensions/DictionaryExtensions.cs
+++ Jakar.Extensions/Extensions/DictionaryExtensions.cs
@@ -21,4 +21,58 @@ public static class DictionaryExtensions
 
         dictionary.Add( key, value );
     }
+    public static TValue GetOrAdd<TKey, TValue>( this Dictionary<TKey, TValue> dictionary, TKey key, TValue value )
+        where TKey : notnull
+    {
+        ref TValue? entry = ref CollectionsMarshal.GetValueRefOrAddDefault( dictionary, key, out bool exists );
+
+        if ( exists )
+        {
+            Debug.Assert( entry is not null );
+            return entry;
+        }
+
+        entry = value;
+        return entry;
+    }
+#if NET9_0_OR_GREATER
+    public static TValue GetOrAdd<TKey, TValue, TAlternateKey>( this Dictionary<TKey, TValue> dictionary, TAlternateKey key, TValue value )
+        where TKey : notnull
+        where TAlternateKey : notnull, allows ref struct
+    {
+        Dictionary<TKey, TValue>.AlternateLookup<TAlternateKey> lookup = dictionary.GetAlternateLookup<TAlternateKey>();
+        ref TValue?                                             entry = ref CollectionsMarshal.GetValueRefOrAddDefault( lookup, key, out bool exists );
+
+        if ( exists )
+        {
+            Debug.Assert( entry is not null );
+            return entry;
+        }
+
+        entry = value;
+        return entry;
+    }
+#endif
+    public static bool TryUpdate<TKey, TValue>( this Dictionary<TKey, TValue> dictionary, TKey key, TValue value )
+        where TKey : notnull
+    {
+        ref TValue entry = ref CollectionsMarshal.GetValueRefOrNullRef( dictionary, key );
+        if ( Unsafe.IsNullRef( ref entry ) ) { return false; }
+
+        entry = value;
+        return true;
+    }
+#if NET9_0_OR_GREATER
+    public static bool TryUpdate<TKey, TValue, TAlternateKey>( this Dictionary<TKey, TValue> dictionary, TAlternateKey key, TValue value )
+        where TKey : notnull
+        where TAlternateKey : notnull, allows ref struct
+    {
+        Dictionary<TKey, TValue>.AlternateLookup<TAlternateKey> lookup = dictionary.GetAlternateLookup<TAlternateKey>();
+        ref TValue                                              entry = ref CollectionsMarshal.GetValueRefOrNullRef( lookup, key );
+        if ( Unsafe.IsNullRef( ref entry ) ) { return false; }
+
+        entry = value;
+        return true;
+    }
+#endif
 }
