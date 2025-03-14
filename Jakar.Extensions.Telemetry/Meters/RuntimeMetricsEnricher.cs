0bb60ce0a1b967661b1aa3c74702e9fa162ac160
Tyler Stegmaier <jakar510@gmail.com>
2025-03-14 15:05:56 -0500

    update nugets.

---
+++ Jakar.Extensions.Telemetry/Meters/RuntimeMetricsEnricher.cs
@@ -0,0 +1,15 @@
+﻿// Jakar.Extensions :: Jakar.Extensions.Telemetry
+// 03/14/2025  15:03
+
+using Serilog.Core;
+using Serilog.Events;
+
+
+
+namespace Jakar.Extensions.Telemetry.Meters;
+
+
+public sealed class RuntimeMetricsEnricher : ILogEventEnricher
+{
+    public void Enrich( LogEvent logEvent, ILogEventPropertyFactory propertyFactory ) { }
+}
