a5c93b5100c698384cdf023b72e1b1d8249dedb7
Tyler Stegmaier <jakar510@gmail.com>
2025-09-26 16:01:43 -0500

    bug fixes

--- TestBlazorMauiApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace TestBlazorMauiApp;
-[Register( "AppDelegate" )]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
