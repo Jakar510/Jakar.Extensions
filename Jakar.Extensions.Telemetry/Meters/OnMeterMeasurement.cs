4be37e0e50b2d9d58c86bfd5d822accd8d25739b
Tyler Stegmaier <jakar510@gmail.com>
2026-01-26 15:58:29 -0600

    move Jakar.Extensions.Telemetry to dedicated repo Jakar.Telemetry

--- Jakar.Extensions.Telemetry/Meters/OnMeterMeasurement.cs
+++
@@ -1,8 +0,0 @@
-﻿// Jakar.Extensions :: Jakar.Extensions.Telemetry
-// 03/13/2025  14:03
-
-namespace Jakar.Extensions.Telemetry.Meters;
-
-
-public delegate void OnMeterMeasurement<TValue>( ref readonly Reading<TValue> value )
-    where TValue : IEquatable<TValue>;
